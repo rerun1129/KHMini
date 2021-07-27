834c76a8c009411bad8adeb09bdec71a11f14526
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-27 16:24:28 +0900

    정상화

--- src/Pull.java
+++ src/Pull.java
@@ -1,11 +1,11 @@
 public class Pull {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("15버전입니다.");
-=======
+
         System.out.println("13버전입니다.");
->>>>>>> parent of ff1dae9 (14버전)
+
     }
 
 }
