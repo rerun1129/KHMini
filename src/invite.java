0c252a7ff9af916f5a658dd69a12e555953ed121
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-27 09:12:42 +0900

    실험중

--- src/invite.java
+++ src/invite.java
@@ -3,7 +3,7 @@ public class invite {
 
     public static void main(String[] args) {
 
-        System.out.println("2조 분들 반갑습니다.");
+        System.out.println("2조 분들 반갑습12니다.");
 
         System.out.println("경!");
     }
