75d01f69e98ea8a1e711b72d001ec5c9ba4e8715
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-07-27 16:06:09 +0900

    돌아가기

--- src/Pull.java
+++ src/Pull.java
@@ -1,8 +1,12 @@
 public class Pull {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("풀 45645364574456요청합니다.");
         System.out.println("풀 고윤석 요청합니다.");
+=======
+        System.out.println("풀 요1223청합니다.");
+>>>>>>> parent of f3df961 (Merge pull request #13 from rerun1129/branchKo)
     }
 
 }
